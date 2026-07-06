d556dcab4af020e6d3eb735c6cf294b7ad5e8b81
Dhanusya-R <dhanusyaragupathi2005dhanu@gmail.com>
2026-07-06 21:34:31 +0530

    Updated App component

--- client/src/components/Home/Layout.tsx
+++ client/src/components/Home/Layout.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import Sidebar from '../Sidebar'
 const Layout =() => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = React.useState(false);
     return (
+        <div className='flex h-screen white'>
+            {/*Mobile Overlay*/}
+            {isMobileMenuOpen && (
+                <div className="fixed inset-0 z-20 bg-black opacity-50 md:hidden"
+                    onClick={() => setIsMobileMenuOpen(false)}
+                ></div>
+            )}
+
+
+            <Sidebar isOpen={isMobileMenuOpen} setIsOpen={setIsMobileMenuOpen} />
         <div>
+            {/*Top bar*/}
+            <header>
 
-            <Sidebar /> 
-
+            </header>
+            <main className="flex-1 overflow-auto p-4 sm:p-6 md:p-8 xl"p-12>
+                <Outlet />
+            </main>
+        </div>
         </div>
     )
 }
