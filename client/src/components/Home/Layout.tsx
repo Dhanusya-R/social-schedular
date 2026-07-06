b8a560463171fa57446f4a7ef21141867cd1029f
Dhanusya-R <dhanusyaragupathi2005dhanu@gmail.com>
2026-07-06 21:36:19 +0530

    Updated App component

--- client/src/components/Home/Layout.tsx
+++ client/src/components/Home/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import Sidebar from '../Sidebar'
+import Sidebar from "./Sidebar";
+import { Outlet } from "react-router-dom";
 const Layout =() => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = React.useState(false);
     return (
